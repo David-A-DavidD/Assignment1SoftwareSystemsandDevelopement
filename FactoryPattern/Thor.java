2372df3a0f5d3dec837a127593b4d63ce338416e
ebube52 <89548621+ebube52@users.noreply.github.com>
2022-03-02 15:31:46 -0800

    Update Thor.java

--- FactoryPattern/Thor.java
+++ FactoryPattern/Thor.java
@@ -1,8 +1,4 @@
 public class Thor implements superhero {
-    public void Identify(){
-        System.out.println("Thor");
-        return;
-    }
 
     public void Weapon(){
         System.out.println("hammer(Mjölnir)");

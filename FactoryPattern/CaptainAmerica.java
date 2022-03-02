4cd3464f5a8a6465f84144c5a5bc8a94d7264554
ebube52 <89548621+ebube52@users.noreply.github.com>
2022-03-02 15:28:38 -0800

    Create CaptainAmerica.java

---
+++ FactoryPattern/CaptainAmerica.java
@@ -0,0 +1,17 @@
+public class CaptainAmerica implements superhero {
+    public void Identify(){
+        System.out.println("CaptainAmerica");
+        return;
+    }
+
+    public void Weapon(){
+        System.out.println("Shield");
+        return;
+    }
+
+    public void Universe(){
+        System.out.println("Marvel");
+        return;
+    }
+
+}

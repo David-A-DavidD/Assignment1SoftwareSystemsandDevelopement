274ec54d78aae45cb72a9afb2a11a1524ad805c7
ebube52 <89548621+ebube52@users.noreply.github.com>
2022-03-02 15:32:04 -0800

    Update CaptainAmerica.java

--- FactoryPattern/CaptainAmerica.java
+++ FactoryPattern/CaptainAmerica.java
@@ -1,9 +1,5 @@
 public class CaptainAmerica implements superhero {
-    public void Identify(){
-        System.out.println("CaptainAmerica");
-        return;
-    }
-
+ 
     public void Weapon(){
         System.out.println("Shield");
         return;
